4ce54bb342bbc607e1ccded7e0888406cd008e3c
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-02-27 09:40:38 +0100

    Delete unused files and update HomeView.vue

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,59 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="container">
+    <div v-if="selectedNumber !== null" class="selected-number">
+      Has presionado el número: #{{ selectedNumber }}
+    </div>
+    <div class="grid">
+      <button v-for="number in numbers" :key="number" @click="showNumber(number)" class="button">
+        {{ number }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      selectedNumber: 0
+    };
+  },
+  methods: {
+    showNumber(number) {
+      this.selectedNumber = number;
+    }
+  }
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+}
+
+.button {
+  padding: 10px;
+  font-size: 18px;
+  background-color: #f0f0f0;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.selected-number {
+  margin-top: 20px;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
